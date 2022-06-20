078ccedfa236e346d2d9f154b9c5c5609a80b0e3
B <vamsi.b.b@capgemini.com>
2022-06-20 17:24:37 +0530

    CRUD operations File Added

---
+++ 1_Learning_Practice/DataBase/MongoDB/3_Basic_CRUD_Operations_Python.py
@@ -0,0 +1,36 @@
+import pymongo
+
+Conn = "mongodb+srv://Vamsi_Batta:12345@cluster0.x5qyupy.mongodb.net/?retryWrites=true&w=majority"
+
+try:
+    client = pymongo.MongoClient(Conn)
+except Exception:
+    print("Error : ", Exception)
+
+myDB = client["Pymongo_CRUD_Operations"]
+
+myCollection = myDB["Document"]
+
+# Insertion
+# Document Contains ID of The Student, Name of a Student, Total Marks he/she Secured
+# myDOC1 = {"_id": 1, "Name": "Abc", "T_Marks": 567}
+# myDOC2 = {"_id": 2, "Name": "Bcd", "T_Marks": 400}
+# myDOC3 = {"_id": 3, "Name": "Cde", "T_Marks": 528}
+# myDOC4 = {"_id": 4, "Name": "Def", "T_Marks": 342}
+
+
+# myCollection.insert_one(myDOC1)
+# myCollection.insert_one(myDOC2)
+# myCollection.insert_one(myDOC3)
+# myCollection.insert_one(myDOC4)
+
+# Reading the Document
+record = myCollection.find_one()
+print(record)
+print("\n\n")
+
+print(client.list_database_names())
+
+# Updating the Record
+
+
