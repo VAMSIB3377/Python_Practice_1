eb502216849b2865e517c03be1aa1c8bf71846b4
B <vamsi.b.b@capgemini.com>
2022-06-20 15:25:39 +0530

    Connected to MongoDB

--- 1_Learning_Practice/DataBase/MongoDB/1_Python_MongoDB_Connection.py
+++ 1_Learning_Practice/DataBase/MongoDB/1_Python_MongoDB_Connection.py
@@ -1,11 +1,15 @@
+# step1 - Install and Import Pymongo
 import pymongo
+
+# step2 - Create a MongoDB Client
 from pymongo import MongoClient
 
-cluster = MongoClient('mongodb+srv://Vamsi_Batta:<12345>@cluster0.x5qyupy.mongodb.net/?retryWrites=true&w=majority')
+try:
+    client = MongoClient("mongodb+srv://Vamsi_Batta:12345@cluster0.x5qyupy.mongodb.net/?retryWrites=true&w=majority")
 
-db = cluster['Test']
-collection = db['Test']
+except Exception:
+    print("Error : ", Exception)
 
-data = {"_id": 0, "Name": "abc", "Score": 5}
 
-collection.insert_one(data)
+
+print(client.list_database_names())
