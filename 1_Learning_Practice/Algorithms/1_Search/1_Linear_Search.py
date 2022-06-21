a6a2e3f987b479d5eecd2ef13a931f9f24d2723d
B <vamsi.b.b@capgemini.com>
2022-06-21 09:50:17 +0530

    Updated The Logic

--- 1_Learning_Practice/Algorithms/1_Search/1_Linear_Search.py
+++ 1_Learning_Practice/Algorithms/1_Search/1_Linear_Search.py
@@ -19,16 +19,22 @@ def Linear_Search(l, a):
 
     for i in range(len(l)):
         if l[i] == a:
-            globals()['pos'] = i
-            return True
+            # globals()['pos'] = i
+            # return True
+            return "Found at index : {} ".format(i)
+
     else:
-        return False
+        return "Not Found"
 
 
 lst = [5, 8, 4, 6, 9, 2]
 n = 6
 
+"""
 if Linear_Search(lst, n):
     print("Found at : ", pos)
 else:
     print("Not Found")
+    
+"""
+print(Linear_Search(lst, n))
