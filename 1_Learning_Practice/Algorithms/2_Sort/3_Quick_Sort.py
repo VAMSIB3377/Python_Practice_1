354d5bd89f9f28344f2ea55ed50f03c021f48dbd
B <vamsi.b.b@capgemini.com>
2022-06-21 10:40:29 +0530

    Implemented Quick Sort and Getting Some Errors

---
+++ 1_Learning_Practice/Algorithms/2_Sort/3_Quick_Sort.py
@@ -0,0 +1,30 @@
+from numpy import array
+
+
+def quick_sort(arr, left, right):
+    if left < right:
+        partition_pos = partition(arr, left, right)
+        quick_sort(arr, left, partition_pos - 1)
+        quick_sort(arr, partition_pos + 1, right)
+
+
+def partition(arr, left, right):
+    i = left
+    j = right
+    pivot = arr(right)
+    while i < j:
+        while i < right and arr[i] < pivot:
+            i += 1
+        while j > left and arr[j] >= pivot:
+            j -= 1
+        if i < j:
+            arr[i], arr[j] = arr[j], arr[i]
+    if arr[i] > pivot:
+        arr[i], arr[right] = arr[right], arr[i]
+    return i
+
+
+if __name__ == "__main__":
+    lst = [63, 45, 87, 94, 38, 2]
+    quick_sort(lst, 0, len(lst)-1)
+    print(lst)
